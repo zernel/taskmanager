9b1ab4381ddd91b54144b3af88220a5aa9cc0b9b
Zernel <itzernel@gmail.com>
2012-12-12 13:03:30 +0800

    Update the views of login and remove signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,8 @@ Taskmanager::Application.routes.draw do
   end
 
   scope constraints: lambda { |r| r.env['warden'].user.nil? } do
-    get "regist", to: "users#new", as: "regist"
     get "login", to: "sessions#new", as: "login"
   end
   delete "logout", to: "sessions#destroy", as: "logout"
-  resources :users
-  resources :sessions
+  resources :sessions, only: :create
 end
