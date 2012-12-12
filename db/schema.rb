6732dc409def5e013059e2db435beced5d225cf9
Zernel <itzernel@gmail.com>
2012-12-12 12:40:59 +0800

    Init the user with warden

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121208022133) do
+ActiveRecord::Schema.define(:version => 20121212024959) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(:version => 20121208022133) do
     t.integer  "type_cd"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "nickname"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
