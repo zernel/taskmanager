0651c9a85de1040d5970f8efc197f1fe003a00e0
Zernel <itzernel@gmail.com>
2012-12-20 20:46:26 +0800

    Add the wysihtml5 editor support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap-wysihtml5
+//= require bootstrap-wysihtml5/locales
 //= require submenu
 //= require events_observer
+//= require wysihtml5
