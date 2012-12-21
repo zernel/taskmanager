9ca0ca20bbfd8a4004301e7471af96d9d6166119
Zernel <itzernel@gmail.com>
2012-12-21 11:43:57 +0800

    optimize tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_filter :save_return_to, only: [:take, :finish, :rollback, :checked]
+  before_filter :save_return_to, :find_task, only: [:take, :finish, :rollback, :checked]
 
   def index
     redirect_to project_tasks_path(Task.last.project) and return unless params[:project_id]
@@ -9,26 +9,27 @@ class TasksController < ApplicationController
   end
 
   def take
-    @task = Task.find(params[:task_id])
     @task.update_attributes(assignee: current_user)
     redirect_to session.delete(:return_to)
   end
 
   def finish
-    @task = Task.find(params[:task_id])
     @task.update_attributes(status: :toreview, assignee: current_user)
     redirect_to session.delete(:return_to)
   end
 
   def rollback
-    @task = Task.find(params[:task_id])
     @task.update_attributes(status: :todo, assignee: nil, creater: current_user)
     redirect_to session.delete(:return_to)
   end
 
   def checked
-    @task = Task.find(params[:task_id])
     @task.update_attributes(status: :done, checker: current_user)
     redirect_to session.delete(:return_to)
+  end
+
+  private
+  def find_task
+    @task = Task.find(params[:task_id])
   end
 end
